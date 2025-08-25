6f028d5331709d4758f89a463170b9b8140960e0
Jacinto <jaclopher@outlook.com>
2025-08-25 05:29:30 +0100

    #1 : add tests for priceService.

--- prices-shared/src/main/java/com/inditex/prices_service/shared/PriceDto.java
+++ prices-shared/src/main/java/com/inditex/prices_service/shared/PriceDto.java
@@ -1,8 +1,15 @@
 package com.inditex.prices_service.shared;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
+@Getter
+@Setter
+@AllArgsConstructor
 public class PriceDto {
     private int productId;
     private int brandId;
@@ -12,23 +19,4 @@ public class PriceDto {
     private BigDecimal price;
     private String curr;
 
-    public PriceDto(int productId, int brandId, int priceList, LocalDateTime startDate, LocalDateTime endDate, BigDecimal price, String curr) {
-        this.productId = productId;
-        this.brandId = brandId;
-        this.priceList = priceList;
-        this.startDate = startDate;
-        this.endDate = endDate;
-        this.price = price;
-        this.curr = curr;
-    }
-
-    // Getters
-    public int getProductId() { return productId; }
-    public int getBrandId() { return brandId; }
-    public int getPriceList() { return priceList; }
-    public LocalDateTime getStartDate() { return startDate; }
-    public LocalDateTime getEndDate() { return endDate; }
-    public BigDecimal getPrice() { return price; }
-    public String getCurr() { return curr; }
 }
-
